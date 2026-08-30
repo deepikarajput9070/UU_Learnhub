fe30ac302d01d92ff3e0cfd2f5b5c31a2324316c
Deepika Rajput <deepikarajput9070@gmail.com>
2026-08-31 00:16:14 +0530

    added course list page and seaarch bar

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import { assets } from '../../assets/assets'
+import SearchBar from './SearchBar'
 
 const Hero = () => {
   return (
@@ -23,6 +23,7 @@ const Hero = () => {
         We bring together world-class instructors, interactive content, and a supportive 
         community to help you achieve your personal and professional goals.
       </p>
+      <SearchBar/>
     </div>
   )
 }
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import Companies from '../../components/student/Companies'
+import CourseSection from '../../components/student/CourseSection'
 import Hero from '../../components/student/Hero'
-
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text center'>
       <Hero />
+      <Companies />
+      <CourseSection />
     </div>
   )
 }
